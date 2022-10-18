39b61da2325b56027cb6e06ac3dac0960c3d6958
Thomas James Crow <thomascrow01@gmail.com>
2022-10-18 15:55:35 +1100

    still need to fix adding correct data

--- src/main/java/org/acme/hibernate/orm/panache/entity/InvoiceEntity.java
+++ src/main/java/org/acme/hibernate/orm/panache/entity/InvoiceEntity.java
@@ -3,7 +3,6 @@ package org.acme.hibernate.orm.panache.entity;
 import java.sql.Date;
 
 import javax.persistence.Cacheable;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
@@ -12,7 +11,6 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 @Cacheable
 public class InvoiceEntity extends PanacheEntity {
 
-    @Column(length = 40, unique = true)
     public int memberID;
     public Date date;
     public int productID;
