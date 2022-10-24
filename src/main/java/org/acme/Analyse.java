da67cf71b08a089e7d0df3d25272c9ab66f108e4
Thomas James Crow <thomascrow01@gmail.com>
2022-10-24 22:29:17 +1100

    added stuff to analyse

--- src/main/java/org/acme/Analyse.java
+++ src/main/java/org/acme/Analyse.java
@@ -1,11 +1,30 @@
 package org.acme;
 
+import org.acme.hibernate.orm.panache.entity.MemberEntity;
 public class Analyse {
     public int memberID;
+    public String memberName;
     public String favouriteProduct;
+    public long totalMoneySpent;
 
-    public Analyse(int memberID, String favouriteProduct) {
-        this.memberID = memberID;
-        this.favouriteProduct = favouriteProduct;
+    public int favouriteProductID;
+    public String favouriteProductName;
+
+    // public Analyse(int memberID, String favouriteProduct) {
+    //     this.memberID = memberID;
+    //     this.favouriteProduct = favouriteProduct;
+    // }
+
+    public Analyse(Long memberID) {
+        this.memberID = memberID.intValue();
+
+        MemberEntity memberEntity = MemberEntity.findById(memberID);
+        memberName = memberEntity.name;
+
     }
+
+    public Analyse(){
+
+    }
+
 }
